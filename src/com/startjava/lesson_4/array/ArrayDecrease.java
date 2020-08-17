14bc6622140add88574671274ec96f54185e38b2
mavic147 <vamaximenko104@gmail.com>
2020-08-17 10:11:53 +0300

    Решение задачи на сжатие массива

---
+++ src/com/startjava/lesson_4/array/ArrayDecrease.java
@@ -0,0 +1,35 @@
+//Сжать массив, удалив из него все элементы из указанного интервала
+package com.startjava.lesson_4.array;
+
+import java.util.Scanner;
+
+public class ArrayDecrease {
+    public static void main(String[] args) {
+        UserArray input = new UserArray();
+        int[] arr = input.getNums();
+        int[] arr2 = new int[input.getLength()];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Print the beginning of the interval: ");
+        int begin = scanner.nextInt();
+        System.out.print("Print the end of the interval: ");
+        int end = scanner.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < begin; i++) {
+            arr2[i] = arr[i];
+            count++;
+        }
+
+        for (int i = end + 1; i < input.getLength(); i++) {
+            arr2[count] = arr[i];
+            count++;
+        }
+
+        System.out.print("New array is: ");
+        for (int i = 0; i < input.getLength(); i++) {
+            System.out.print(arr2[i] + " ");
+        }
+    }
+}
+
