7f0e005b4cc8122e5c0e83efe96c65babb9fcfd2
mavic147 <vamaximenko104@gmail.com>
2020-08-22 14:43:42 +0300

    digit sum changed

--- src/com/startjava/lesson_4/array/ArrayDigitSum.java
+++ src/com/startjava/lesson_4/array/ArrayDigitSum.java
@@ -7,32 +7,23 @@ public class ArrayDigitSum {
     public static void main(String[] args) {
         UserArray input = new UserArray();
         int[] arr = input.getNums();
-        String[] str = new String[input.getLength()];
-//        ArrayList<Integer> list = new ArrayList<Integer>();
-
-        int lastValue;
         int sumDigits = 0;
-        for (int i = 0; i < input.getLength(); i++) {
-            while (arr[i] != 0) {
-                lastValue = arr[i] % 10;
-                sumDigits += lastValue;
-                arr[i]  /= 10;
+        for (int el: arr){
+            for(char digit: String.valueOf(el).toCharArray()){
+                sumDigits += Character.getNumericValue(digit);
             }
         }
+
+//        int lastValue;
+//        for (int i = 0; i < input.getLength(); i++) {
+//            while (arr[i] != 0) {
+//                lastValue = arr[i] % 10;
+//                sumDigits += lastValue;
+//                arr[i]  /= 10;
+//            }
+//        }
+
         System.out.println("Sum of each element's digit is: " + sumDigits);
 
-//        for (int i = 0; i < input.getLength(); i++) {
-//            String text = String.join(",", String.valueOf(arr[i]).split(""));
-//            str[i] = text;
-//        }
-//
-//        for (int i = 0; i < input.getLength(); i++) {
-//              list.add(Integer.parseInt(str[i]));
-//        }
-//
-//        int sumDigits = 0;
-//        for (int i = 0; i < list.size(); i++) {
-//            sumDigits += list.get(i);
-//        }
     }
 }
